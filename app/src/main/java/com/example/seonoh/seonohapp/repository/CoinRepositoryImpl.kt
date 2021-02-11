c91c7943dfb37da314ed9298cadd54017622b202
Seonoh Eam <eso0609@naver.com>
2021-02-11 19:29:44 +0900

    summary data api

--- app/src/main/java/com/example/seonoh/seonohapp/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/seonoh/seonohapp/repository/CoinRepositoryImpl.kt
@@ -2,6 +2,10 @@ package com.example.seonoh.seonohapp.repository
 
 import com.example.seonoh.seonohapp.datasource.CoinDataSourceImpl
 
+// 서버 통신 정보 구현
+// 데이터 전송 담당
+// 필요 통신 구현
+
 class CoinRepositoryImpl : CoinRepository {
 
     private val coinDataSource = CoinDataSourceImpl()
