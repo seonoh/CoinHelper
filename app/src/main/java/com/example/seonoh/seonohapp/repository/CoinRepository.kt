7829ac7556052c82edfcf65d83d4527da8edc1eb
Seonoh Eam <eso0609@naver.com>
2021-02-11 19:29:56 +0900

    summary data api

--- app/src/main/java/com/example/seonoh/seonohapp/repository/CoinRepository.kt
+++ app/src/main/java/com/example/seonoh/seonohapp/repository/CoinRepository.kt
@@ -4,6 +4,9 @@ import com.example.seonoh.seonohapp.model.CurrentPriceInfoModel
 import com.example.seonoh.seonohapp.model.Market
 import io.reactivex.Single
 
+// 서버 통신 정보 interface
+// 필요 통신 정리
+
 interface CoinRepository {
     fun sendMarket() : Single<List<Market>>
     fun sendCurrentPriceInfo(
